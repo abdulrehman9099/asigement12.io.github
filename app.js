7c38b040b5f0c7eea8782f1b4b57af47ff772a55
abdulrehman9099 <115347902+abdulrehman9099@users.noreply.github.com>
2022-10-13 03:16:50 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.querySelector(".link").style.right = "0px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.querySelector(".link").style.right = "-200px";
+    document.getElementById("main").style.marginLeft= "0";
+  }
